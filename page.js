4c59cacea3e0a29a20a9c164b461dee11f61709d
chooboy <chooboy05@gmail.com>
2025-01-30 10:15:58 +1300

    set up map bounds and zoom

--- page.js
+++ page.js
@@ -1,8 +1,11 @@
-var map = L.map('map').setView([-40.9006, 174.8860], 5);
+var map = L.map('map').setView([-41.2883, 174.7666], 12);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
+            minZoom: 10,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+map.setMaxBounds([[-41.15177509803096, 175.3280641410647], [-41.42985785717913, 174.2143251273928]])
 
 function NameToCircle(cityname) {
     var result;
@@ -26,4 +29,10 @@ function NameToCircle(cityname) {
     }
     console.log(result)
     return result;
+}
+
+function giveBounds() {
+    var bounds = map.getBounds();
+    console.log(bounds);
+    return bounds;
 }
