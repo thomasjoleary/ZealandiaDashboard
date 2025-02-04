1b7975317aac5efb5b478c61110d76e747115569
grantkortfelt <grantmail3301@gmail.com>
2025-02-05 10:40:38 +1300

    Update dot size

--- visualize.js
+++ visualize.js
@@ -1,8 +1,8 @@
 console.log("d3 Visualization Version:", d3.version);
 
 // set dimensions of the canvas / graph
-const xSize = 400;
-const ySize = 400;
+const xSize = 500;
+const ySize = 500;
 const margin = 20;
 
 // create svg element
@@ -40,14 +40,14 @@ const axisBottom = ySize * 0.90;
 // create axes
 // create x axis
 const xScale = d3.scaleLinear()
-    .domain([0, d3.max(xdata)*1.1])
+    .domain([0, d3.max(xdata)])
     .range([axisLeft, axisRight]);
 svg.append("g")
     .attr("transform", "translate(0, " + axisRight + ")")
     .call(d3.axisBottom(xScale));
 // x axis label
 svg.append("text")
-    .attr("transform", "translate(" + (xSize/2) + " ," + (ySize) + ")")
+    .attr("transform", "translate(" + (xSize/2) + " ," + ySize + ")")
     .style("text-anchor", "middle")
     .text("Month");
 
@@ -69,22 +69,24 @@ svg.append("text")
 
 
 // add dots to svg
+const smallDot = xSize / 90;
+const bigDot = xSize / 35;
 svg.selectAll(".dot")
     .data(ydata)
     .enter().append("circle")
     .attr("class", "dot")
     .attr("cx", (d, i) => xScale(xdata[i]))
     .attr("cy", (d, i) => yScale(ydata[i]))
-    .attr("r", 5)
+    .attr("r", smallDot)
     .on("mouseover", function(d, i) {
         d3.select(this).transition()
             .duration('750')
-            .attr('r', 12.5)
+            .attr('r', bigDot)
             .attr('fill', 'red');
     })
     .on("mouseout", function(d, i) {
         d3.select(this).transition()
             .duration('750')
-            .attr('r', 5)
+            .attr('r', smallDot)
             .attr('fill', 'black');
     });
