5d70aad5020a2da57743f9e9a16f9fd43e74226d
grantkortfelt <grantmail3301@gmail.com>
2025-02-14 11:31:00 +1300

    Create historicaldata.js
    Placeholder data

---
+++ assets/historicaldata.js
@@ -0,0 +1,71 @@
+const historicaldata =
+{
+    "locations": [
+        {
+            "name": "Appleton Park",
+            "lat": -41.260735,
+            "lng": 174.789888,
+            "timeline": [
+                {
+                    "year": 1929,
+                    "month": null,
+                    "day": null,
+                    "event": "The Appleton Park area before dumping was a wooded valley.",
+                    "img": [
+                        {
+                            "src": "assets/images/historicaldata/appleton/1929.jpg",
+                            "alt": "Appleton Park before dumping."
+                        }
+                    ]
+                },
+                {
+                    "year": 1935,
+                    "month": null,
+                    "day": null,
+                    "event": "Dumping begins at Appleton Park landfill. Toxic wastes include heavy metals, asbestos, and other hazardous materials.",
+                    "img": [
+                        {
+                            "src": "assets/images/historicaldata/appleton/1935.jpg",
+                            "alt": "Appleton Park dumping in 1935."
+                        }
+                    ]
+                },
+                {
+                    "year": 2025,
+                    "month": 2,
+                    "day": 12,
+                    "event": "Appleton Park landfill covered over with grass, a parking lot, and a cycle path.",
+                    "img": [
+                        {
+                            "src": "assets/images/historicaldata/appleton/2025.jpg",
+                            "alt": "Appleton Park in 2025 pictured from the south."
+                        },
+                        {
+                            "src": "assets/images/historicaldata/appleton/2025-2.jpg",
+                            "alt": "Appleton Park in 2025 from a different angle."
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            "name": "Estuary",
+            "lat": -41.285393,
+            "lng": 174.754128,
+            "timeline": [
+                {
+                    "year": 2025,
+                    "month": null,
+                    "day": null,
+                    "event": "The Kaiwharawhara estuary is not reachable by civilians. The rail line and the motorway cut off access.",
+                    "img": [
+                        {
+                            "src": "assets/images/historicaldata/estuary/2025.jpg",
+                            "alt": "Estuary in 2025, pictured upstream of the rail line."
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
